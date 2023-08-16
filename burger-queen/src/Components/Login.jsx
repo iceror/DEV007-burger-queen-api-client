c07d18fa1682d985c16ffb8aa242e3b0c5129c04
iceror <ilsecervor@gmail.com>
2023-08-16 17:39:09 -0600

    import css && create form

--- burger-queen/src/Components/Login.jsx
+++ burger-queen/src/Components/Login.jsx
@@ -1,7 +1,20 @@
+import '../css/build.css'
+
 const Login = () => {
   return (
     <>
-      <h2>Burger Queen</h2>
+      <h1>Burger Queen</h1>
+      <form className='login-form'>
+        <div>
+          <label htmlFor="user">usuario</label>
+          <input type="text" name="user" />
+        </div>
+        <div>
+          <label htmlFor="password">contraseña</label>
+          <input type="text" name="password" />
+        </div>
+        <button type="submit">iniciar</button>
+      </form>
     </>
   )
 }
