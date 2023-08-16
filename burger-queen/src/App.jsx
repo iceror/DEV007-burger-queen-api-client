9f799a8854f927e6efbfe3ce25a0facae3eece7a
iceror <ilsecervor@gmail.com>
2023-08-16 13:27:24 -0600

    import css build

--- burger-queen/src/App.jsx
+++ burger-queen/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './css/build.css'
+import Login from './Components/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h2>Burger Queen</h2>
+      <Login/>
     </>
   )
 }
