a33ca28362c5fbfad166480d107d4ac798d0ac58
iceror <ilsecervor@gmail.com>
2023-08-20 22:28:10 -0600

    add useState to handle usr/passchanges, call getAuth

--- burger-queen/src/Components/Login.jsx
+++ burger-queen/src/Components/Login.jsx
@@ -1,19 +1,41 @@
+import { useState } from 'react'
+import { getAuth } from '../api-fn/api-utils'
 import '../css/build.css'
 
 const Login = () => {
+  //use useState or useRef to get data from inputs
+  const [user, setUser] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleUser = (event) => {
+    setUser(event.target.value)
+  }
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleClick = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    // Here, you can perform actions like sending the data to a server for authentication
+    console.log('Username:', user);
+    console.log('Password:', password);
+    getAuth(user, password)
+  }
+
   return (
     <>
       <h1>Burger Queen</h1>
       <form className='login-form'>
         <div>
           <label htmlFor="user">usuario</label>
-          <input type="text" name="user" />
+          <input type="text" name="user" value={user} onChange={handleUser}/>
         </div>
         <div>
           <label htmlFor="password">contraseña</label>
-          <input type="text" name="password" />
+          <input type="password" name="password" value={password} onChange={handlePassword}/>
         </div>
-        <button type="submit">iniciar</button>
+        <button type='button' onClick={handleClick}>iniciar</button>
       </form>
     </>
   )
