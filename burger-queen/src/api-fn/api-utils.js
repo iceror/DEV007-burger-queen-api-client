6f3582d0e692e7ec449615769015dcda5e3ab78a
iceror <ilsecervor@gmail.com>
2023-08-20 22:27:05 -0600

    create js file for api fn

---
+++ burger-queen/src/api-fn/api-utils.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+export const getAuth = (user, password) => {
+  console.log('inside getAuth');
+  let data = JSON.stringify({
+    "email": user,
+    "password": password
+    // "email": "anita.borg@systers.xyz",
+    // "password": "123456"
+  });
+
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'http://localhost:8080/login',
+    //url: 'https://virtserver.swaggerhub.com/ssinuco/BurgerQueenAPI/2.0.0/login',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data: data
+  };
+
+  axios.request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
