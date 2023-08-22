a0bb84d9890ff25651a4a2544b3bf8fe0bb5415e
iceror <ilsecervor@gmail.com>
2023-08-22 17:59:52 -0600

    add routes for orders && admin panel

--- burger-queen/src/App.jsx
+++ burger-queen/src/App.jsx
@@ -1,9 +1,10 @@
 import './css/build.css'
 import Login from './Components/Login'
 import CreateOrders from './Components/CreateOrders'
+import Orders from './Components/Orders'
 import { UserProvider } from './context/UserContext'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-Route
+import AdminPanel from './Components/AdminPanel'
 
 function App() {
   return (
@@ -13,8 +14,10 @@ function App() {
           <Routes>
             <Route path='/' element={<Login />} />
             <Route path='/create-orders' element={<CreateOrders />} />
+            <Route path='/orders' element={<Orders />} />
+            <Route path='/admin-panel' element={<AdminPanel />} />
+
           </Routes>
-          {/* otros componentes */}
         </>
       </UserProvider>
     </BrowserRouter>
