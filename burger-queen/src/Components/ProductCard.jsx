0a8baf92c7e4baedcca0bfb2d9b8f3b997abae60
iceror <ilsecervor@gmail.com>
2023-08-22 22:36:47 -0600

    create product card file

---
+++ burger-queen/src/Components/ProductCard.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { getProducts } from "../api-fn/api-utils";
+import { UserContext } from "../context/UserContext";
+
+const ProductCard = () => {
+  const { user, sendUserToContext } = useContext(UserContext);
+  console.log(user);
+
+  const renderProducts = async () => {
+    let products = await getProducts(user.accessToken)
+    console.log(products);
+    products.forEach(product => {
+      
+      console.log(product);
+    })
+  }
+
+  renderProducts()
+
+  return (
+      <>
+      Product cards 
+      </>
+    )
+}
+
+export default ProductCard
