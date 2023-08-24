37612524b38456ed62759630ab778a8e5c16b4a0
iceror <ilsecervor@gmail.com>
2023-08-23 23:18:27 -0600

    add class for styles

--- burger-queen/src/Components/ProductCard.jsx
+++ burger-queen/src/Components/ProductCard.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { getProducts } from "../api-fn/api-utils";
 import { UserContext } from "../context/UserContext";
 import '../css/build.css'
+import QuantityCounter from "./QuantityCounter";
 
 const ProductCard = ({ products }) => {
   const { user, sendUserToContext } = useContext(UserContext);
@@ -13,10 +14,11 @@ const ProductCard = ({ products }) => {
       {products.map(product =>
         <div className="card">
           <img src={product.image} alt="" />
-          <div>
+          <div className="text">
             <p>{product.name}</p>
             <p>{product.price}</p>
           </div>
+          <QuantityCounter/>
         </div>)
       }
     </>
