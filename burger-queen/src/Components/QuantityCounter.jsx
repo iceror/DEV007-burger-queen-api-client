8ac54575af7cd9483c759912a01e0cfe365f5472
iceror <ilsecervor@gmail.com>
2023-08-23 23:18:59 -0600

    create quantity counter

---
+++ burger-queen/src/Components/QuantityCounter.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import '../css/build.css'
+
+
+const QuantityCounter = () => {
+  const [count, setCount] = useState(1)
+
+  const substract = () => {
+    count > 1 ? setCount(count - 1) : count;
+  }
+
+  const add = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <div className="counter">
+      <button onClick={substract} className="counter-button">-</button>
+      <p>{count}</p>
+      <button onClick={add} className="counter-button">+</button>
+    </div>
+  )
+}
+
+export default QuantityCounter
