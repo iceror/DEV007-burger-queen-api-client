58721e51f10ed00a34e271848f710006c3731018
iceror <ilsecervor@gmail.com>
2023-08-29 10:39:23 -0600

    create firt test

--- burger-queen/src/tests/Login.spec.jsx
+++ burger-queen/src/tests/Login.spec.jsx
@@ -1,8 +1,11 @@
 import { render } from "@testing-library/react"
 import { Login } from '../Components/Login'
+import { MemoryRouter, Routes } from "react-router"
 
 describe('first', () => {
   test('should be a match with snapshot', () => {
-    render(<Login/>)
+    render(<MemoryRouter>
+      <Login />
+    </MemoryRouter>)
   })
 })
