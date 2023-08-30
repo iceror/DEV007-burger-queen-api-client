2aaa130cc8d6c3429d752294a7e57da3e0ea3a6c
iceror <ilsecervor@gmail.com>
2023-08-30 11:13:12 -0600

    handle add to add products

--- burger-queen/src/Components/ProductCard.jsx
+++ burger-queen/src/Components/ProductCard.jsx
@@ -1,30 +1,49 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { getProducts } from "../api-fn/api-utils";
 import { UserContext } from "../context/UserContext";
 import '../css/build.css'
 import QuantityCounter from "./QuantityCounter";
+import { OrderContext } from "../context/OrderContext";
 
 const ProductCard = ({ products }) => {
   // const { user, sendUserToContext } = useContext(UserContext);
   // console.log(user);
   // console.log(products);
+  const [quantity, setQuantity] = useState(1);
+  const { addToOrder } = useContext(OrderContext)
+
+  const handleQuantityChange = (newQuantity) => {
+    setQuantity(newQuantity)
+  }
+
+  const handleAdd = (product) => {
+    let id = product.id;
+    let productName = product.name;
+    let productPrice = product.price;
+    const addedProduct = {
+      id,
+      productName,
+      productPrice,
+      quantity: quantity
+    }
+    // console.log(addedProduct);
+    addToOrder(addedProduct)
+  }
 
   return (
     <>
       {products.map(product =>
-        <div className="card" key={product.id}> 
+        <div className="card" key={product.id} onClick={() => handleAdd(product)}>
           <img src={product.image} alt="" />
           <div className="text">
             <p>{product.name}</p>
             <p>{product.price}</p>
           </div>
-          <QuantityCounter/>
+          <QuantityCounter onQuantityChange={handleQuantityChange} />
         </div>)
       }
     </>
   )
-
-
 }
 
 export default ProductCard
