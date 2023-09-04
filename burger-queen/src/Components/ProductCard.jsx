e1116efaa1c17a8eb0d46de47dac87551c9e500b
iceror <ilsecervor@gmail.com>
2023-09-04 11:28:28 -0600

    delete handleQuantityChange

--- burger-queen/src/Components/ProductCard.jsx
+++ burger-queen/src/Components/ProductCard.jsx
@@ -5,41 +5,21 @@ import '../css/build.css'
 import QuantityCounter from "./QuantityCounter";
 import { OrderContext } from "../context/OrderContext";
 
-const ProductCard = ({ products }) => {
-  // const { user, sendUserToContext } = useContext(UserContext);
-  // console.log(user);
+const ProductCard = ({ products, onCardClick }) => {
   // console.log(products);
-  const [quantity, setQuantity] = useState(1);
-  const { addToOrder } = useContext(OrderContext)
 
-  const handleQuantityChange = (newQuantity) => {
-    setQuantity(newQuantity)
-  }
-
-  const handleAdd = (product) => {
-    let id = product.id;
-    let productName = product.name;
-    let productPrice = product.price;
-    const addedProduct = {
-      id,
-      productName,
-      productPrice,
-      quantity: quantity
-    }
-    // console.log(addedProduct);
-    addToOrder(addedProduct)
-  }
+  
 
   return (
     <>
       {products.map(product =>
-        <div className="card" key={product.id} onClick={() => handleAdd(product)}>
+        <div className="card" key={product.id} >
           <img src={product.image} alt="" />
           <div className="text">
             <p>{product.name}</p>
             <p>{product.price}</p>
           </div>
-          <QuantityCounter onQuantityChange={handleQuantityChange} />
+          <QuantityCounter product={product} />
         </div>)
       }
     </>
