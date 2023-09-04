fce83280202bc1219cf655b78bf4bcca2c08dd1a
iceror <ilsecervor@gmail.com>
2023-09-04 11:28:02 -0600

    fix quantity bug when count = 0

--- burger-queen/src/Components/QuantityCounter.jsx
+++ burger-queen/src/Components/QuantityCounter.jsx
@@ -1,26 +1,42 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import '../css/build.css'
+import { OrderContext } from "../context/OrderContext"
 
-const QuantityCounter = ({onQuantityChange}) => {
-  const [count, setCount] = useState(1)
+const QuantityCounter = ({ product }) => {
+  const [count, setCount] = useState(0)
+  const { addToOrder, deleteFromOrder } = useContext(OrderContext)
+
 
   const substract = () => {
-    if (count > 1) {
+    if (count > 0) {
       const newCount = count - 1;
       setCount(newCount);
-      onQuantityChange(newCount); // Pass the updated count
+      update(newCount)
     }
   }
 
   const add = () => {
     const newCount = count + 1;
-    console.log(newCount);
     setCount(newCount);
-    onQuantityChange(newCount); 
+    update(newCount)
   }
 
+  const update = (newCount) => {
+    if (newCount === 0) {
+      deleteFromOrder(product.id);
+    } else {
+      const addedProduct = {
+        id: product.id,
+        productName: product.name,
+        productPrice: product.price,
+        count: newCount,
+      };
+      addToOrder(addedProduct);
+    }
+  };
+
   return (
-    <div className="counter">
+    <div className="counter" >
       <button onClick={substract} className="counter-button">-</button>
       <p>{count}</p>
       <button onClick={add} className="counter-button">+</button>
