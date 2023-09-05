4c50726bd085dbbfbb05cf4c2e6f18dae8b3fcfd
iceror <ilsecervor@gmail.com>
2023-09-04 22:12:41 -0600

    call get orders fn in cmp

--- burger-queen/src/Components/Orders.jsx
+++ burger-queen/src/Components/Orders.jsx
@@ -1,17 +1,35 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { UserContext } from "../context/UserContext"
 import Sidebar from "./Sidebar";
+import { getOrders } from "../api-fn/api-utils";
 
 const Orders = () => {
   const { user } = useContext( UserContext);
-  console.log(user);
+  const [ orders, setOrders] = useState([])
+  // console.log(user);
+
+  const fetchOrders = async () => {
+    const fetchedOrders = await getOrders(user.accessToken)
+    console.log(fetchedOrders);
+    setOrders(fetchedOrders)
+  }
+  
+  useEffect(() => {
+    fetchOrders()
+  }, []);
+
+
+  // TO DO create timer from order entry to when it is done 
+
 
   if(user.user.role === 'cook'){
     return (
       <div className="background">
         <div className="orders">
           <h2>Burger Queen</h2>
-          <div className="orders" id="orders"></div>
+          <div className="orders" id="orders">
+            <OrderCards orders={orders}/>
+          </div>
           <Sidebar></Sidebar>
         </div>
       </div>
