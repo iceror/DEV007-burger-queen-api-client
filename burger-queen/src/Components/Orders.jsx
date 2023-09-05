282eda2838f1f5f3c159a285070a676637744b86
iceror <ilsecervor@gmail.com>
2023-09-04 22:59:21 -0600

    conditional to render order cards

--- burger-queen/src/Components/Orders.jsx
+++ burger-queen/src/Components/Orders.jsx
@@ -2,33 +2,35 @@ import { useContext, useEffect, useState } from "react"
 import { UserContext } from "../context/UserContext"
 import Sidebar from "./Sidebar";
 import { getOrders } from "../api-fn/api-utils";
+import OrderCards from "./OrderCards"
 
 const Orders = () => {
-  const { user } = useContext( UserContext);
-  const [ orders, setOrders] = useState([])
+  const { user } = useContext(UserContext);
+  const [orders, setOrders] = useState([])
   // console.log(user);
 
   const fetchOrders = async () => {
     const fetchedOrders = await getOrders(user.accessToken)
-    console.log(fetchedOrders);
     setOrders(fetchedOrders)
+    // console.log(fetchedOrders);
   }
-  
+
   useEffect(() => {
     fetchOrders()
   }, []);
 
-
   // TO DO create timer from order entry to when it is done 
 
-
-  if(user.user.role === 'cook'){
+  if (user.user.role === 'cook') {
     return (
       <div className="background">
         <div className="orders">
           <h2>Burger Queen</h2>
-          <div className="orders" id="orders">
-            <OrderCards orders={orders}/>
+          <div className="orders-container" >
+            { orders.length > 0 ?
+              <OrderCards orders={orders} /> :
+              <p>No hay órdenes pendientes 😄</p>
+            }
           </div>
           <Sidebar></Sidebar>
         </div>
